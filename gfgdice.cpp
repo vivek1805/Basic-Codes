34901b276f8af06afae123fc2724555c5ef449ba
VIVEK KUMAR <42572336+vivek1805@users.noreply.github.com>
2018-09-18 20:37:56 +0530

    The dice problem

---
+++ gfgdice.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //code
+    int i,T,a;
+    cin>>T;
+    for(i=0;i<T;i++)
+    {
+        cin>>a;
+        cout<<7-a<<endl;
+    }
+    return 0;
+}
